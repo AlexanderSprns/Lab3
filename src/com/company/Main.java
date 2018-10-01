51a43001de29d233787152b67d47c56761e43f06
AlexanderSprns <GITHUB alex.sprns@gmail,com>
2018-10-01 22:02:49 +0300

    /

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        double temp = 0.0;
         for (int n=2; n<10; n++){
             for (int a=1; a<100; a++) {
                 for (int b=1; b<100; b++) {
                     for (int c=1; c<100; c++) {
-                        if (Math.pow(a,n)+ Math.pow(b,n)== Math.pow(c,n)) {
-                            System.out.println("n = " + n + ", " + (long)Math.pow(a, n) +" + " + (long)Math.pow(b, n)
-                                    +" = "+ (long)Math.pow(c, n));
+                        temp = Math.pow(a,n)+ Math.pow(b,n);
+                        if (Math.abs(temp - Math.pow(c,n)) < 0.00001) {
+                            System.out.println("n = " + n + ", " + Math.pow(a, n) +" + " + Math.pow(b, n)
+                                    +" = "+ Math.pow(c, n));
                         }
                     }
                 }
             }
         }
-       double positive = 0;
+       /*double positive = 0;
        double negative = 0;
        double result;
        for (int i=1; i<=10000; i++) {
@@ -27,5 +29,6 @@ public class Main {
        }
        result = positive - negative;
         System.out.println("Second task \nResult: " + result);
+        */
     }
 }
